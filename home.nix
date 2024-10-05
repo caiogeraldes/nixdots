664d2721c52efedd0e9c2d6c906411d6c80fb5d0
Caio Geraldes <caio.geraldes@usp.br>
2024-10-05 11:16:53 -0300

    add: xdg folders

--- home.nix
+++ home.nix
@@ -6,6 +6,18 @@ in {
   # manage.
   home.username = "caiog";
   home.homeDirectory = "/home/caiog";
+  xdg.userDirs = {
+    enable = true;
+    createDirectories = true;
+    desktop="${config.home.homeDirectory}/desktop";
+    download="${config.home.homeDirectory}/down";
+    templates="${config.home.homeDirectory}/templates";
+    documents="${config.home.homeDirectory}/docs";
+    music="${config.home.homeDirectory}/mus";
+    pictures="${config.home.homeDirectory}/img";
+    videos="${config.home.homeDirectory}/vids";
+    publicShare="${config.home.homeDirectory}/pub";
+  };
 
   home.packages = with pkgs; [
     zsh
