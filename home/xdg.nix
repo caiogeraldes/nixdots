40c712f93f41c8b972edcaae660a9f74d76a28da
Caio Geraldes <caio.geraldes@usp.br>
2024-11-01 12:26:44 -0300

    add: mimeapps

--- home/xdg.nix
+++ home/xdg.nix
@@ -1,15 +1,23 @@
 { config, pkgs, inputs, ... }:
 {
-  xdg.userDirs = {
-    enable = true;
-    createDirectories = true;
-    desktop="${config.home.homeDirectory}/desktop";
-    download="${config.home.homeDirectory}/down";
-    templates="${config.home.homeDirectory}/templates";
-    documents="${config.home.homeDirectory}/docs";
-    music="${config.home.homeDirectory}/mus";
-    pictures="${config.home.homeDirectory}/img";
-    videos="${config.home.homeDirectory}/vids";
-    publicShare="${config.home.homeDirectory}/pub";
+  xdg = {
+    userDirs = {
+      enable = true;
+      createDirectories = true;
+      desktop="${config.home.homeDirectory}/desktop";
+      download="${config.home.homeDirectory}/down";
+      templates="${config.home.homeDirectory}/templates";
+      documents="${config.home.homeDirectory}/docs";
+      music="${config.home.homeDirectory}/mus";
+      pictures="${config.home.homeDirectory}/img";
+      videos="${config.home.homeDirectory}/vids";
+      publicShare="${config.home.homeDirectory}/pub";
+    };
+    mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "application/pdf" = ["org.pwmt.zathura.desktop"];
+      };
+    };
   };
 }
