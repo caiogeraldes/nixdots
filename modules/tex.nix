b40db40e2c903d2057dbcc69d91824478192fea5
Caio Geraldes <caio.geraldes@usp.br>
2024-11-04 12:35:07 -0300

    add: pecita font

--- modules/tex.nix
+++ modules/tex.nix
@@ -2,6 +2,7 @@
 with pkgs;
 {
   environment.systemPackages = with pkgs; [
+    pecita
     ((texlive.combine {
       inherit (texlive) scheme-full;
       pkgFilter = pkg: lib.elem pkg.tlType [ "run" "bin" "doc" ];
