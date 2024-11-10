4176274ed3cb063b8508b7c97f03407b21600527
Caio Geraldes <caio.geraldes@usp.br>
2024-11-10 16:41:26 -0300

    add: stan treesitter

--- configs/nvim/lua/custom/plugins/treesitter-stan.lua
+++ configs/nvim/lua/custom/plugins/treesitter-stan.lua
@@ -3,7 +3,7 @@ return {
   config = function()
     require('nvim-treesitter.parsers').get_parser_configs().stan = {
       install_info = {
-        url = '~/proj/tree-sitter-stan/', -- local path or git repo
+        url = 'https://github.com/WardBrian/tree-sitter-stan?tab=readme-ov-file', -- local path or git repo
         files = { 'src/parser.c' },
         branch = 'main',
       },
