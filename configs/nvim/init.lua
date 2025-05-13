306a4a036dfdb3f467a1200136e7bf0af7d0f4ff
Caio Geraldes <caio.geraldes@usp.br>
2025-05-13 15:44:42 -0300

    updt

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -383,6 +383,8 @@ require('lazy').setup {
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        latex = { 'latexindent' },
+        tex = { 'latexindent' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
