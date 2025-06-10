352460e1df69ae61bf350aaafc0ac1a290789e57
Caio Geraldes <caio.geraldes@usp.br>
2025-06-10 08:39:24 -0300

    rethinking

--- scripts/rethinking.R
+++ scripts/rethinking.R
@@ -1,4 +1,6 @@
 Sys.setenv(DOWNLOAD_STATIC_LIBV8 = 1)
 install.packages("V8")
 install.packages("dagitty")
+devtools::install_github("stan-dev/cmdstanr")
+cmdstanr::install_cmdstan()
 devtools::install_github("rmcelreath/rethinking")
